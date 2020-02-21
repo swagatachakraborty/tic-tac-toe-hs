fd00bcfe218a658754e35a63bc1cea14c3381bd3
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2020-02-21 12:46:04 +0530

    WIP

--- main.hs
+++ main.hs
@@ -4,31 +4,16 @@ import Data.List
 type Board = ([Position],[Position])
 
 data Symbol = X | O deriving (Show,Eq)
-data Position = Zero | One | Two | Three | Four | Five | Six | Seven | Eight deriving(Show,Eq,Enum)
+data Position = Zero | One | Two | Three | Four | Five | Six | Seven | Eight | Invalid deriving(Show,Eq,Enum)
 data Player = Player {name :: String, symbol :: Symbol} deriving (Show,Eq)
 data GameState = Setup | InPlay Player Player Board | HasWon Player | Drawn deriving(Show,Eq)
 
-updateBoard :: Board -> Position -> Symbol -> Board
-updateBoard (xs, os) p X = (p : xs, os)
-updateBoard (xs, os) p O = (xs, p:os)
-
-playGame :: GameState -> String
-playGame (HasWon p) = (name p) ++ " has won"
-playGame Drawn = "game is drawn"
-playGame (InPlay pNow pNext (xs, os))
--- move
-  |(hasWon xs) == True = playGame (HasWon pNow)
-  |(hasWon os) == True = playGame (HasWon pNext)
-  |(length xs + length os) == 9 = playGame Drawn
-  |otherwise = playGame (InPlay pNext pNow (xs, os))
-
-startGame :: IO String
-startGame = do 
-  putStrLn "Player 1 enter your name :"
-  p1Name <- getLine
-  putStrLn "Player 2 enter your name :"
-  p2Name <- getLine
-  return (playGame $ InPlay (Player p1Name X) (Player p2Name O) ([],[]))
+mapPosition :: String -> Position
+mapPosition charCh
+  | choice < (length choices) = choices !! choice
+  | otherwise = Invalid
+  where choices = [Zero .. Eight]
+        choice = read charCh
 
 diagonalComb = [[Zero,Four,Eight], [Two,Four,Six]]
 rowComb = chunksOf 3 [Zero .. Eight]
@@ -41,8 +26,37 @@ isPresent a b = b `elem` a
 hasWon :: [Position] -> Bool
 hasWon moves = any (all (isPresent moves)) winComb
 
+updateBoard :: Board -> Symbol -> Position -> Board
+updateBoard (xs, os) sm p
+  | sm == X = (p : xs, os)
+  | sm == O = (xs, p:os)
+  |otherwise = (xs, os)
+
+playGame :: GameState -> IO String
+playGame (HasWon p) = return ((name p) ++ " has won")
+playGame Drawn = return ("game is drawn")
+playGame (InPlay pNow pNext (xs, os))
+  |((hasWon xs) || (hasWon os)) == True = playGame (HasWon pNext)
+  |(length xs + length os) == 9 = playGame Drawn
+  |otherwise = 
+    ((InPlay pNext pNow) <$> ((updateBoard (xs, os) (symbol pNow)) <$> (makeMove pNow))) >>= playGame
+
+makeMove :: Player -> IO Position
+makeMove p = do 
+  putStrLn ((name p) ++ " enter your move :")
+  choice <- getLine
+  return (mapPosition choice)
+
+startGame :: IO String
+startGame = do 
+  putStrLn "Player 1 enter your name :"
+  p1Name <- getLine
+  putStrLn "Player 2 enter your name :"
+  p2Name <- getLine
+  playGame $ InPlay (Player p1Name X) (Player p2Name O) ([],[])
+
 main :: IO ()
 main = do 
-  putStrLn "Started"
+  putStrLn "-------------------Started-------------------"
   game <- startGame
   putStrLn game
